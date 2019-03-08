20d836e4824d913cba4c94ced4820124bbad547e
Andrew Jesaitis <andrew@andrewjesaitis.com>
2019-03-08 10:08:06 -0800

    Make emacs transparent

--- customizations/ui.el
+++ customizations/ui.el
@@ -101,3 +101,22 @@
 
 ;; no bell
 (setq ring-bell-function 'ignore)
+
+;;Transparency
+;; Make emacs transparent
+(set-frame-parameter (selected-frame) 'alpha '(85 . 50))
+(add-to-list 'default-frame-alist '(alpha . (85 . 50))))
+
+;; Allow transparency to be toggled
+ (defun toggle-transparency ()
+   (interactive)
+   (let ((alpha (frame-parameter nil 'alpha)))
+     (set-frame-parameter
+      nil 'alpha
+      (if (eql (cond ((numberp alpha) alpha)
+                     ((numberp (cdr alpha)) (cdr alpha))
+                     ;; Also handle undocumented (<active> <inactive>) form.
+                     ((numberp (cadr alpha)) (cadr alpha)))
+               100)
+          '(85 . 50) '(100 . 100)))))
+ (global-set-key (kbd "C-c t") 'toggle-transparency)
