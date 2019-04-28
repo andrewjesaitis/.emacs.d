514cfbef53b7066c089fce3c50ac990973483c08
Andrew Jesaitis <andrew@andrewjesaitis.com>
2019-04-28 10:00:16 -0700

    Python options

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -3,7 +3,7 @@
       python-shell-interpreter-args "console --simple-prompt"
       python-shell-prompt-detect-failure-warning nil)
 
-;;enable elpy 
+;;enable elpy
 (elpy-enable)
 (setq elpy-rpc-backend "jedi")
 (setq elpy-test-pytest-runner-command (quote ("pytest -n 7 -m \"not slow\"")))
@@ -11,4 +11,22 @@
 
 (add-hook
  'elpy-mode-hook
- '(lambda () (setq-local ac-max-width 0.5)))  
+ '(lambda () (setq-local ac-max-width 0.5)))
+
+;; Python debugger toggle
+(defun toggle-pdb ()
+  "Toggle import pbd; pdb.set_trace() at current line"
+  (interactive)
+  (let ((x (line-number-at-pos))
+    (cur (point)))
+    (search-forward-regexp "^[ ]*import pdb; pdb.set_trace();")
+    (if (= x (line-number-at-pos))
+        (let ()
+          (move-beginning-of-line 1)
+          (kill-line 1)
+          (move-beginning-of-line 1))
+      (let ()
+        (move-beginning-of-line 1)
+        (insert "import pdb; pdb.set_trace();\n")))))
+
+(local-set-key (kbd "C-c d") 'toggle-pdb)
