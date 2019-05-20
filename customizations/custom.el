1824300c9e79da1cd689c59f78c5f710914bd929
Andrew Jesaitis <andrew@andrewjesaitis.com>
2019-05-20 16:00:04 -0700

    Use newer version of theme

--- customizations/custom.el
+++ customizations/custom.el
@@ -4,12 +4,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
+ '(custom-safe-themes
+   (quote
+    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
  '(package-selected-packages
    (quote
-    (package-lint yaml-mode web-mode tern-auto-complete tagedit smex scss-mode rainbow-delimiters projectile paredit markdown-mode magit json-mode js2-mode ido-ubiquitous haskell-mode git-gutter flycheck exec-path-from-shell elpy dockerfile-mode coffee-mode clojure-mode-extra-font-locking cider ag ack-and-a-half))))
+    (color-theme-sanityinc-tomorrow org-tracktable package-lint yaml-mode web-mode tern-auto-complete tagedit smex scss-mode rainbow-delimiters projectile paredit markdown-mode magit json-mode js2-mode ido-ubiquitous haskell-mode git-gutter flycheck exec-path-from-shell elpy dockerfile-mode coffee-mode clojure-mode-extra-font-locking cider ag ack-and-a-half))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(magit-diff-added ((t (:background "black" :foreground "green3"))))
+ '(magit-diff-removed ((t (:background "black" :foreground "red3")))))
--- customizations/ui.el
+++ customizations/ui.el
@@ -44,14 +44,7 @@
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 
 ;; Color Themes
-;; Read http://batsov.com/articles/2012/02/19/color-theming-in-emacs-reloaded/
-;; for a great explanation of emacs color themes.
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Custom-Themes.html
-;; for a more technical explanation.
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(add-to-list 'load-path "~/.emacs.d/themes")
-;; (load-theme 'spacegray t)
-(load-theme 'tomorrow-night-bright t)
+(require 'color-theme-sanityinc-tomorrow)
 
 ;; powerline customizes the mode line
 ;; (add-to-list 'load-path "~/.emacs.d/vendor/emacs-powerline")
