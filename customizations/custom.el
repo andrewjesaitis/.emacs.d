5126aaa55c8e2ace9523ad5f4b09518d35ac20b2
Andrew Jesaitis <andrew@andrewjesaitis.com>
2019-07-03 11:35:32 -0700

    Update Elpy

--- customizations/custom.el
+++ customizations/custom.el
@@ -8,10 +8,14 @@
  '(custom-safe-themes
    (quote
     ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+ '(elpy-test-runner (quote elpy-test-pytest-runner))
  '(global-evil-colemak-basics-mode t)
  '(package-selected-packages
    (quote
-    (ac-rtags general vigenere which-key evil-leader key-chord evil-colemak-basics evil auto-compile yatemplate org-plus-contrib color-theme-sanityinc-tomorrow org-tracktable package-lint yaml-mode web-mode tern-auto-complete tagedit smex scss-mode rainbow-delimiters projectile paredit markdown-mode json-mode js2-mode ido-ubiquitous haskell-mode git-gutter flycheck exec-path-from-shell elpy dockerfile-mode coffee-mode clojure-mode-extra-font-locking cider ag ack-and-a-half)))
+    (evil-org evil-escape evil-collection aa-edit-mode ac-rtags general vigenere which-key evil-leader key-chord evil-colemak-basics evil auto-compile yatemplate org-plus-contrib color-theme-sanityinc-tomorrow org-tracktable package-lint yaml-mode web-mode tern-auto-complete tagedit smex scss-mode rainbow-delimiters projectile paredit markdown-mode json-mode js2-mode ido-ubiquitous haskell-mode git-gutter flycheck exec-path-from-shell elpy dockerfile-mode coffee-mode clojure-mode-extra-font-locking cider ag ack-and-a-half)))
+ '(python-shell-internal-buffer-name "Jupyter")
+ '(python-shell-interpreter "jupyter")
+ '(python-shell-interpreter-args "console --simple-prompt")
  '(which-key-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -1,8 +1,3 @@
-;;jupyter as an interactive python backend
-(setq python-shell-interpreter "jupyter"
-      python-shell-interpreter-args "console --simple-prompt"
-      python-shell-prompt-detect-failure-warning nil)
-
 ;;enable elpy
 (elpy-enable)
 (setq elpy-rpc-ignored-buffer-size 10024000)
@@ -20,20 +15,47 @@
 ;;  '(lambda ()
 ;;     (setq-local ac-max-width 0.5)))
 
-;; Python debugger toggle
-(defun toggle-pdb ()
-  "Toggle import pbd; pdb.set_trace() at current line"
-  (interactive)
-  (let ((x (line-number-at-pos))
-    (cur (point)))
-    (search-forward-regexp "^[ ]*import pdb; pdb.set_trace();")
-    (if (= x (line-number-at-pos))
-        (let ()
-          (move-beginning-of-line 1)
-          (kill-line 1)
-          (move-beginning-of-line 1))
-      (let ()
-        (move-beginning-of-line 1)
-        (insert "import pdb; pdb.set_trace();\n")))))
-
-(local-set-key (kbd "C-c d") 'toggle-pdb)
+(general-define-key
+ :states 'motion
+ :keymaps 'elpy-mode
+ :prefix "SPC"
+   "" nil
+ 
+   "e"  '(:ignore t :which-key "elpy")
+   "en" '(:ignore t :which-key "navigation")
+   "end" 'xref-find-definitions
+   "enp" 'xref-pop-marker-stack
+   "enr" 'xref-find-references
+   "ena" 'xref-find-apropos
+   "eno" 'elpy-occur-definitions
+   "es"  '(:ignore t :which-key "shell")
+   "ess" 'elpy-shell-switch-to-shell
+   "esl" 'elpy-shell-set-local-shell
+   "esk" 'elpy-shell-kill
+   "esd" 'elpy-shell-kill-all
+   "el"  '(:ignore t :which-key "load-to-shell")
+   "ell" 'elpy-shell-send-statement-and-step
+   "els" 'elpy-shell-send-statement
+   "elg" 'elpy-shell-send-statement-and-go
+   "elx" 'elpy-shell-send-statement-and-step-and-go
+   "elg" '(:ignore t :which-key "group")
+   "elgg" 'elpy-shell-send-group-and-step
+   "elgl" 'elpy-shell-send-group
+   "elgx" 'elpy-shell-send-group-and-step-and-go
+   "elgc" 'elpy-shell-send-codecell-and-step
+   "elr" 'elpy-shell-send-region-or-buffer
+   "elf" 'elpy-shell-send-defun
+   "elc" 'elpy-shell-send-defclass
+   ;; TODO: Add other shell-send commands
+   "ee" 'elpy-flymake-next-error
+   "ep" 'elpy-flymake-previous-error
+   "eh" '(:ignore t :which-key "help")
+   "ehd" 'elpy-doc
+   "ed" '(:ignore t :which-key "debug")
+   "edt" 'elpy-pdb-toggle-breakpoint-at-point
+   "edb" 'elpy-pdb-debug-buffer
+   "edp" 'elpy-pdb-break-at-point
+   "ede" 'eply-pdb-debug-last-exception
+   "ef" 'elpy-format-code
+   "er" 'elpy-refactor
+   "ea" 'elpy-multi-edit-python-symbol-at-point)
