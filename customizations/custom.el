4e6251fceabf54d6453954fc7d7c147f189978f7
Andrew Jesaitis <andrew@andrewjesaitis.com>
2019-12-10 21:18:07 -0800

    Remove extra custom configs

--- customizations/custom.el
+++ customizations/custom.el
@@ -3,21 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(global-company-mode t)
- '(package-selected-packages
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(custom-safe-themes
    (quote
-    (ws-butler use-package yaml-mode which-key web-mode tern-auto-complete tagedit scss-mode rainbow-delimiters projectile paredit org-plus-contrib markdown-mode magit json-mode js2-mode godoctor git-gutter general flycheck-gometalinter exec-path-from-shell evil-org evil-escape evil-collection evil-colemak-basics elpy dockerfile-mode counsel company-go color-theme-sanityinc-tomorrow clojure-mode-extra-font-locking cider ag)))
- '(python-shell-internal-buffer-name "Jupyter")
- '(python-shell-interpreter "jupyter")
- '(python-shell-interpreter-args "console --simple-prompt")
- '(reb-re-syntax (quote string))
- '(safe-local-variable-values (quote ((pyvenv-workon . zwork3))))
- '(which-key-mode t))
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(hl-sexp-background-color "#efebe9"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#eaeaea" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 125 :width normal :foundry "ADBO" :family "Source Code Pro"))))
- '(magit-diff-added ((t (:background "black" :foreground "green3"))))
- '(magit-diff-removed ((t (:background "black" :foreground "red3")))))
+ )
