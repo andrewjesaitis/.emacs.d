fea03e9127dc416b88c41871e008356bc3cbda3e
Andrew Jesaitis <andrew@andrewjesaitis.com>
2020-03-01 11:27:19 -0800

    Fix refile of new task bug

--- customizations/setup-org.el
+++ customizations/setup-org.el
@@ -216,7 +216,7 @@
   (interactive)
   (save-excursion
     (beginning-of-line 0)
-    (org-remove-empty-drawer-at "LOGBOOK" (point))))
+    (org-remove-empty-drawer-at (point))))
 
 (add-hook 'org-clock-out-hook 'avj/remove-empty-drawer-on-clock-out 'append)
 
